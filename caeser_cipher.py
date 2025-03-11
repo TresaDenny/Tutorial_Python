28b6efdb7942ae0dffa48752165391addb1ed9bb
Tresa Maria Denny <tresamade@gmail.com>
2025-03-11 22:55:12 +0530

    caeser_cipher.py

--- caeser_cipher.py
+++ caeser_cipher.py
@@ -1,25 +1,26 @@
-message=input("Enter a message : ")
-key=int(input("Enter key code : "))
-
-ciphertext=""
-for ch in message:
-    ordinal_ch=ord(ch)
-    ordinal_ch+=key
-    if ordinal_ch > ord('z'):
-        ordinal_ch=ord('a')+(ordinal_ch-ord('z')-1)
-    new_ch=chr(ordinal_ch)
-    ciphertext+=new_ch
-print(ciphertext)
-
-
-decrypted_message = ""
-
-for ch in ciphertext:
-    ordinal_ch = ord(ch)
-    ordinal_ch -= key
-    if ordinal_ch < ord('a'):
-        ordinal_ch = ord('z') - (ord('a') - ordinal_ch-1 )
-    new_ch = chr(ordinal_ch)
-    decrypted_message += new_ch
-
-print("Decrypted message:", decrypted_message)
+inputstr = input("Enter the input string:")
+ key = int(input("Enter the key value:"))
+ encrypt_text = ""
+ 
+ for val in inputstr:
+     ordinalval = ord(val)
+     ordinalval += key
+     if ordinalval > ord('z'):
+         ordinalval = ord('a') + ordinalval - ord('z') -1
+     codevalue = chr(ordinalval)
+     encrypt_text += codevalue
+ print("Encrypted text:",encrypt_text)
+ 
+ #Decryption
+ outputstr = input("Eter the output string:")
+ key = int(input("Enter the key value:"))
+ decrypt_text = ""
+ 
+ for val in outputstr:
+     ordinalval = ord(val)
+     ordinalval -= key
+     if ordinalval < ord('a'):
+         ordinalval = ord('z') - ordinalval - ord('a') -1
+     codevalue = chr(ordinalval)
+     decrypt_text += codevalue
+ print("Decrypted text:",decrypt_text)
